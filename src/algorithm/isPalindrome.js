6e0796510f893cf221e1b328bcd33bc7877d0b5b
赵江涛 <9043448+zhaopjiangtao521@user.noreply.gitee.com>
2025-06-11 22:19:32 +0800

    feat(algorithm):leetCode->13->罗马数转数字

--- src/algorithm/isPalindrome.js
+++ src/algorithm/isPalindrome.js
@@ -1,5 +1,23 @@
+// 力扣  判断是不是回文
 var isPalindrome = function (x) {
     var arr = (" " + x).split("").reverse();
-    return arr.join("") == (" " + x).split("").join("");
+    return arr.join("") == ("" + x).split("").join("");
 };
-console.log(isPalindrome(121));
+var isPalindrome2 = function (x) {
+    var str = x.toString();
+    var res = true;
+    var l = 0;
+    var r = str.length - 1;
+    while (r > l) {
+        console.log(str.charAt(l), str.charAt(r));
+        if (str.charAt(l) != str.charAt(r)) {
+            res = false;
+            break;
+        }
+        l++;
+        r--;
+    }
+    return res;
+};
+// console.log(isPalindrome(121))
+console.log(isPalindrome2(10));
--- src/algorithm/isPalindrome.ts
+++ src/algorithm/isPalindrome.ts
@@ -4,4 +4,25 @@ var isPalindrome = function (x:any):boolean {
     return arr.join("") == (""+x).split("").join("")
 }
 
-console.log(isPalindrome(121))
+
+// 简单题复杂做法， 双指针移动 如果移动完后不一致即为非回文数
+var isPalindrome2 = function (x:Number):boolean {
+    var str:String = x.toString();
+    var res :boolean = true
+    var l:number =0
+    var r :number = str.length-1
+    while (r>l){
+        console.log(str.charAt(l),str.charAt(r));
+        if(str.charAt(l)!=str.charAt(r)){
+            res = false
+            break;
+        }
+        l++
+        r--
+    }
+    return res
+}
+
+// console.log(isPalindrome(121))
+
+console.log(isPalindrome2(10));
